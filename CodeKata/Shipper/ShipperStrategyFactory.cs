40881fb6a379b1368b27e052d87534c89777a544
Oliver <oliver@yezz.com.tw>
2021-04-20 21:20:48 +0800

    feat: 新增使用 Func 實作運費計算

--- CodeKata/Shipper/ShipperStrategyFactory.cs
+++ CodeKata/Shipper/ShipperStrategyFactory.cs
@@ -1,5 +1,6 @@
 using System;
 using CodeKata.Enum;
+using CodeKata.Model;
 
 namespace CodeKata.Shipper
 {
@@ -15,6 +16,35 @@ namespace CodeKata.Shipper
                     return new HsinchuShipper();
                 case ShipperType.PostOffice:
                     return new PostOfficeShipper();
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(shipperType), shipperType,
+                        "Shipper type is not exist");
+            }
+        }
+
+        public static Func<Product, double> GetShippingFeeFunction(ShipperType shipperType)
+        {
+            switch (shipperType)
+            {
+                case ShipperType.BlackCat:
+                    return (product) => product.Weight > 20 ? 500 : 100 + product.Weight * 10;
+                case ShipperType.Hsinchu:
+                    return (product) =>
+                    {
+                        var size = product.Length * product.Width * product.Height;
+                        if (product.Length > 100 || product.Width > 100 || product.Height > 100)
+                            return size * 0.00002 * 1100 + 500;
+                        else
+                            return size * 0.00002 * 1200;
+                    };
+                case ShipperType.PostOffice:
+                    return (product) =>
+                    {
+                        var feeByWeight = 80 + product.Weight * 10;
+                        var size = product.Length * product.Width * product.Height;
+                        var feeBySize = size * 0.00002 * 1100;
+                        return feeByWeight < feeBySize ? feeByWeight : feeBySize;
+                    };
                 default:
                     throw new ArgumentOutOfRangeException(nameof(shipperType), shipperType,
                         "Shipper type is not exist");
